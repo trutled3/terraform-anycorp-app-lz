675836472ef869ff2178189a61eef5b1f23915db
Travis Rutledge <travis.rutledge@ibm.com>
2026-01-28 16:43:09 -0500

    adding aws provider

--- examples/anycorp/app456-dev/main.tf
+++ examples/anycorp/app456-dev/main.tf
@@ -3,6 +3,8 @@
 # https://developer.hashicorp.com/terraform/cloud-docs/workspaces/run/run-environment#environment-variables
 variable "TFC_WORKSPACE_NAME" {}
 
+provider "aws" {}
+
 resource "aws_s3_bucket" "example" {
   bucket = "vault-backed-dynamic-creds-example-bucket-${var.TFC_WORKSPACE_NAME}"
 
