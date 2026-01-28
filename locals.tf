470aaa45a9eb6ee0de9048ceb0f599342f77f9f0
Travis Rutledge <travis.rutledge@ibm.com>
2026-01-28 15:58:41 -0500

    fixing local ref

--- locals.tf
+++ locals.tf
@@ -4,9 +4,9 @@ locals {
   workspace_keys = {
     for k, w in tfe_workspace.workspace :
     k => {
-      workspace_name = w.name
-      workspace_id = w.id
-      workspace_vault_role_name = "${var.app_name}-tfc-${w.name}-reader-role"
+      workspace_name            = w.name
+      workspace_id              = w.id
+      workspace_vault_role_name = "${var.app_name}-tfe-${w.name}-reader-role"
     }
   }
 
